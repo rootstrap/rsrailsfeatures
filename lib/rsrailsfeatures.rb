fdd257d261ae1c4d0be91af7c17ce362fca6db23
fedeagripa <federicogagripa@gmail.com>
2020-04-30 23:40:54 -0300

    initial commit from rsgem template

---
+++ lib/rsrailsfeatures.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'rsrailsfeatures/version'
+
+module Rsrailsfeatures
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rsrailsfeatures/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rsrailsfeatures
+  VERSION = '0.1.0'
+end
